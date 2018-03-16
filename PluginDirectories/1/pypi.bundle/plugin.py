f2c3464f0944ab2d4dda87f44ab3b62d8dfe9dbd
Atomic Coding <atomicgaming4life@gmail.com>
2018-03-15 21:42:41 -0700

    Added Options Again

--- PluginDirectories/1/pypi.bundle/plugin.py
+++ PluginDirectories/1/pypi.bundle/plugin.py
@@ -1,3 +1,4 @@
+import json
 import urllib
 
 
@@ -11,14 +12,25 @@ def results(parsed, original_query):
     if len(query) < 2:
         return
 
-    url_template = 'https://warehouse.python.org/search/?q={0}'
+    # Load server settings
+    with open('preferences.json') as f:
+        settings = json.load(f)
+    server = settings.get('server', None)
+
+    # Set server URL
+    if server == 'warehouse':
+        url_template = 'https://warehouse.python.org/search/?q={0}'
+    elif server == 'devpi':
+        url_template = 'http://localhost:3141/+search?query={0}'
+    else:
+        url_template = 'https://pypi.python.org/pypi?:action=search&term={0}'
 
     url = url_template.format(urllib.quote_plus(query))
     html = """
     <script>
     setTimeout(function () {
         window.location = '""" + url + """';
-    }, 100);
+    }, 500);
     </script>
     """
 
@@ -28,3 +40,7 @@ def results(parsed, original_query):
         "html": html,
         "webview_links_open_in_browser": True
     }
+
+def run(url):
+    import os
+    os.system('open "{0}"'.format(url))
